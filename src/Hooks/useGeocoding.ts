d06ac8b87a25e70a75dd00c01990e3a189266ab4
sovla <sovla@naver.com>
2022-05-25 16:19:04 +0900

    - 지역설정 변경 완료

--- src/Hooks/useGeocoding.ts
+++ src/Hooks/useGeocoding.ts
@@ -7,7 +7,22 @@ function useGeocoding(region: {latitude: number; longitude: number} | null) {
     const {i18n} = useTranslation();
     const [isLoading, setIsLoading] = useState(false);
     const [location, setLocation] = useState<null | {
-        results: Array<any> | undefined;
+        results: {
+            address_components: {
+                long_name: string;
+                short_name: string;
+                types: string[];
+            }[];
+            formatted_address: string;
+            geometry: {
+                location: {
+                    lat: number;
+                    lng: number;
+                };
+            };
+            place_id: string;
+            types: string[];
+        }[];
     }>(null);
 
     const refTimer = useRef<any>(null);
@@ -17,14 +32,21 @@ function useGeocoding(region: {latitude: number; longitude: number} | null) {
             setIsLoading(true);
             const config: any = {
                 method: 'get',
-                url: `https://maps.googleapis.com/maps/api/geocode/json?latlng=${region.latitude},${region.longitude}&key=AIzaSyAbfTo68JkJSdEi9emDHyMfGl7vxjYD704&language=${geoLanguage(
-                    'br', //i18n.language
-                )}&result_type=street_address|political|country`,
+                url: `https://maps.googleapis.com/maps/api/geocode/json`,
+                params: {
+                    latlng: `${region.latitude},${region.longitude}`,
+                    key: 'AIzaSyAbfTo68JkJSdEi9emDHyMfGl7vxjYD704',
+                    language: geoLanguage(
+                        'br', //i18n.language
+                    ),
+                    result_type: 'route|sublocality_level_1|administrative_area_level_2|administrative_area_level_1|country',
+                },
                 headers: {},
             };
             axios(config)
                 .then(function (response) {
-                    console.log(response?.data);
+                    console.log('geocoder Revers :::', response?.data);
+                    // return;
                     if (response.data.status === 'OK') {
                         setLocation(response.data);
                     }
@@ -45,29 +67,28 @@ function useGeocoding(region: {latitude: number; longitude: number} | null) {
     let detail = '';
 
     if (location !== null && Array.isArray(location.results)) {
-        let find = false;
-        const length = location.results.length;
-
-        locationName = location.results ? location.results[length - 3].formatted_address : '';
-        detail = location !== null ? location.results[1].formatted_address : '';
+        type locationDataKey = 'route' | 'sublocality_level_1' | 'administrative_area_level_2' | 'administrative_area_level_1' | 'country';
+        const findItemList: locationDataKey[] = ['route', 'sublocality_level_1', 'administrative_area_level_2', 'administrative_area_level_1', 'country'];
+        const locationData = {
+            route: '',
+            sublocality_level_1: '',
+            administrative_area_level_2: '',
+            administrative_area_level_1: '',
+            country: '',
+        };
 
         for (const result of location.results) {
-            if (Array.isArray(result.address_components)) {
-                for (const address of result.address_components) {
-                    if (Array.isArray(address.types)) {
-                        for (const type of address.types) {
-                            if (type === 'sublocality_level_1') {
-                                city = address.long_name;
-                                find = true;
-                            }
-                            if (find) break;
-                        }
+            for (const address of result.address_components) {
+                for (const type of address.types) {
+                    if (findItemList.find(v => type === v)) {
+                        locationData[type as locationDataKey] = type !== 'administrative_area_level_1' ? address.long_name + ' ' : address.short_name;
                     }
-                    if (find) break;
                 }
             }
-            if (find) break;
         }
+        locationName = locationData.sublocality_level_1 + locationData.administrative_area_level_2 + locationData.administrative_area_level_1 + locationData.country;
+        detail = locationData.route + locationData.sublocality_level_1 + locationData.administrative_area_level_2 + locationData.administrative_area_level_1;
+        city = locationData.sublocality_level_1;
     }
 
     return {location, detail, city, locationName, isLoading};
